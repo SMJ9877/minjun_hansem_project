557de680467429dcf5704be53964745becca3cc2
SMJ9877 <guds9876@naver.com>
2020-09-01 16:56:17 +0900

    Revert "Revert "rt""
    This reverts commit f5a5dcb25e600d1ef58f3699470cb610e82aa9bf.

--- First_project/Hello_world.cpp
+++ First_project/Hello_world.cpp
@@ -3,10 +3,13 @@ using namespace std;
 
 int main(void)
 {
+<<<<<<< HEAD
+    cout << "난 왜 안되냐고" << endl;
+=======
 
     cout << 1 << "Hello World" << endl;
 
     cout << "Hello2hey" << endl;
+>>>>>>> 1a332b116e2551806925624c0a528f233de46794
 
-    return 0;
 }
---
+++ Ican/Ican/vehicle.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <string>
+
+class vehicle
+{
+public:
+	std::string vehicleNum;
+};
